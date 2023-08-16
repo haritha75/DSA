9239fa40b75ecc1385ae110ebad40269099ea0b7
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-08-16 09:08:54 +0530

    Day14 work done

--- Day14/FractionEx.java
+++ Day14/FractionEx.java
@@ -18,26 +18,45 @@ class Fraction {
 
     void multiply(int numeratorA, int denominatorA, int numeratorB, int denominatorB) {
 
-        int Numerator = numeratorA * numeratorB;
-        int Denominator = denominatorA * denominatorB;
-        System.out.println("Multiplication: " + Numerator + "/" + Denominator);
+        if(numeratorA==0||numeratorB==0) {
+            System.out.println("Multiplication: "+0);
+        }
+        else if(denominatorA==0||denominatorB==0) {
+            System.out.println("Multiplication: infinity");
+        }
+       
+        else if(numeratorA!=0||denominatorA!=0||numeratorB!=0||numeratorB!=0) {
+
+            int Numerator = numeratorA * numeratorB;
+            int Denominator = denominatorA * denominatorB;
+            System.out.println("Multiplication: " + Numerator + "/" + Denominator);
+        }
+      
     }
 
     void divide(int numeratorA, int denominatorA, int numeratorB, int denominatorB) {
 
-        if (numeratorB != 0) {
+         if(numeratorA==0||denominatorB==0) {
+            System.out.println("division :"+0);
+         }
+         else if(denominatorA==0||numeratorB==0) {
+            System.out.println("division :infinity");
+         }
+
+         else if(numeratorA!=0||denominatorA!=0||numeratorB!=0||numeratorB!=0) {
+
             int Numerator = numeratorA * denominatorB;
             int Denominator = denominatorA * numeratorB;
             System.out.println("divison: " + Numerator + "/" + Denominator);
-        } else {
-            System.out.println("Cannot divide by zero.");
-        }
+         }
+        
+        
     }
 }
 
 public class FractionEx {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the numerator of fraction a: ");
