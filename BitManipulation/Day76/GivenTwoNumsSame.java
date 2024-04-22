31e231f3494e8e07415a38451e3fafcccaa71320
haritha75 <yerukonduharitha@gmail.com>
2024-04-22 21:56:37 +0530

    DSA work updated

---
+++ BitManipulation/Day76/GivenTwoNumsSame.java
@@ -0,0 +1,13 @@
+public class GivenTwoNumsSame {
+
+    public static void main(String[] args) {
+        
+        int n=10,n1=9;
+
+        if((n^n1)==0)
+           System.out.println("Equal");
+        else
+          System.out.println("not equal");   
+    }
+    
+}
